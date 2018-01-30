c79259635ea1807cbf49182936932b0872dd3bb8
Артём <hoodsey20@gmail.com>
2018-01-30 21:15:30 +0700

    правки от наставника

--- js/stat.js
+++ js/stat.js
@@ -11,22 +11,21 @@ var PADDING = 20;
 var TEXT_FONT_SIZE = 16;
 var TEXT_FONT_FAMILY = 'PT Mono';
 var TEXT_FONT_COLOR = '#000';
+var TEXT_LINE_HEIGHT = Math.round(TEXT_FONT_SIZE * 1.3);
 // константы для колонок статистики
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-
-var textLineHeight = Math.round(TEXT_FONT_SIZE * 1.3);
-var barHeight = CLOUD_HEIGHT - PADDING * 3 - textLineHeight * 4;
+var BAR_HEIGHT = CLOUD_HEIGHT - PADDING * 3 - TEXT_LINE_HEIGHT * 4;
 // PADDING * 3, а не на 2, потому что нам нужен дополнительный отступ после заголовка
 
-var renderCloud = function (ctx, props) {
-  if (props.isShadowNeeded) {
-    ctx.fillStyle = props.shadowColor;
-    ctx.fillRect(props.x + CLOUD_GAP, props.y + CLOUD_GAP, props.width, props.height);
-  }
+var renderCloud = function (ctx, x, y, width, height, color, shadowColor) {
+  // рисуем тень
+  ctx.fillStyle = shadowColor;
+  ctx.fillRect(x + CLOUD_GAP, y + CLOUD_GAP, width, height);
 
-  ctx.fillStyle = props.color;
-  ctx.fillRect(props.x, props.y, props.width, props.height);
+  // рисуем popup
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
 };
 
 var renderText = function (ctx, text, x, y) {
@@ -44,58 +43,47 @@ var getRounded = function (element) {
   return Math.round(element);
 };
 
-var getRandomOpacity = function () {
-  return Math.ceil(Math.random() * 10) / 10;
+var getChartColumnColor = function (name) {
+  var color = 'rgba(255, 0, 0, 1)';
+  if (name !== 'Вы') {
+    color = 'rgba(42, 0, 254, ' + Math.ceil(Math.random() * 10) / 10 + ')';
+  }
+  return color;
 };
 
-var renderChartColumn = function (ctx, props) {
-  ctx.fillStyle = props.color;
-  ctx.fillRect(props.x, props.y, props.width, props.height);
+var renderChartColumn = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
 };
 
 window.renderStatistics = function (ctx, names, times) {
+  var nameTextYcoordinate = CLOUD_HEIGHT - PADDING;
   // округляем до целых результаты игроков
   var playersTimeResults = times.map(getRounded);
   // находим максимальное значение из результатов игроков
   var maxTime = getMaxOfArray(playersTimeResults);
-  var cloudProps = {
-    x: CLOUD_X,
-    y: CLOUD_Y,
-    width: CLOUD_WIDTH,
-    height: CLOUD_HEIGHT,
-    color: '#fff',
-    isShadowNeeded: true,
-    shadowColor: 'rgba(0, 0, 0, 0.3)',
-  };
 
   // рисуем popup с результатами
-  renderCloud(ctx, cloudProps);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff', 'rgba(0, 0, 0, 0.3)');
 
   // рисуем заголовок для popup с результатами
   renderText(ctx, 'Ура вы победили!', CLOUD_X + PADDING, CLOUD_Y + PADDING);
-  renderText(ctx, 'Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + textLineHeight);
+  renderText(ctx, 'Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + TEXT_LINE_HEIGHT);
 
   for (var i = 0; i < names.length; i++) {
     var columnLeftMargin = (BAR_WIDTH + BAR_GAP) * i;
-    var nameTextYcoordinate = CLOUD_HEIGHT - PADDING;
     var columnXcoordinate = CLOUD_X + PADDING + columnLeftMargin;
-    var columnHeight = playersTimeResults[i] * barHeight / maxTime;
+    var columnHeight = playersTimeResults[i] * BAR_HEIGHT / maxTime;
     var columnYCoordinate = CLOUD_HEIGHT - columnHeight - (PADDING + TEXT_FONT_SIZE);
-    var chartColor = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(42, 0, 254, ' + getRandomOpacity() + ')';
-    var chartColumnProps = {
-      color: chartColor,
-      x: columnXcoordinate,
-      y: columnYCoordinate,
-      width: BAR_WIDTH,
-      height: columnHeight,
-    };
+    var chartColor = getChartColumnColor(names[i]);
+
     // выводим имена игроков
     renderText(ctx, names[i], columnXcoordinate, nameTextYcoordinate);
 
     // рисуем колонки
-    renderChartColumn(ctx, chartColumnProps);
+    renderChartColumn(ctx, columnXcoordinate, columnYCoordinate, BAR_WIDTH, columnHeight, chartColor);
 
     // выводим результат над колонками
-    renderText(ctx, playersTimeResults[i], columnXcoordinate, columnYCoordinate - textLineHeight);
+    renderText(ctx, playersTimeResults[i], columnXcoordinate, columnYCoordinate - TEXT_LINE_HEIGHT);
   }
 };
