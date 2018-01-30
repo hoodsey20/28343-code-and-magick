97980cbb992e94b40b1da3ccb33e5d87515db741
Артём <hoodsey20@gmail.com>
2018-01-30 20:26:53 +0700

    вынес отрисовку объектов и рандомный opacity в отдельные функции

--- js/stat.js
+++ js/stat.js
@@ -19,9 +19,14 @@ var textLineHeight = Math.round(TEXT_FONT_SIZE * 1.3);
 var barHeight = CLOUD_HEIGHT - PADDING * 3 - textLineHeight * 4;
 // PADDING * 3, а не на 2, потому что нам нужен дополнительный отступ после заголовка
 
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var renderCloud = function (ctx, props) {
+  if (props.isShadowNeeded) {
+    ctx.fillStyle = props.shadowColor;
+    ctx.fillRect(props.x + CLOUD_GAP, props.y + CLOUD_GAP, props.width, props.height);
+  }
+
+  ctx.fillStyle = props.color;
+  ctx.fillRect(props.x, props.y, props.width, props.height);
 };
 
 var renderText = function (ctx, text, x, y) {
@@ -39,14 +44,34 @@ var getRounded = function (element) {
   return Math.round(element);
 };
 
+var getRandomOpacity = function () {
+  return Math.ceil(Math.random() * 10) / 10;
+};
+
+var renderChartColumn = function (ctx, props) {
+  ctx.fillStyle = props.color;
+  ctx.fillRect(props.x, props.y, props.width, props.height);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   // округляем до целых результаты игроков
   var playersTimeResults = times.map(getRounded);
   // находим максимальное значение из результатов игроков
   var maxTime = getMaxOfArray(playersTimeResults);
+  var cloudProps = {
+    x: CLOUD_X,
+    y: CLOUD_Y,
+    width: CLOUD_WIDTH,
+    height: CLOUD_HEIGHT,
+    color: '#fff',
+    isShadowNeeded: true,
+    shadowColor: 'rgba(0, 0, 0, 0.3)',
+  };
 
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  // рисуем popup с результатами
+  renderCloud(ctx, cloudProps);
+
+  // рисуем заголовок для popup с результатами
   renderText(ctx, 'Ура вы победили!', CLOUD_X + PADDING, CLOUD_Y + PADDING);
   renderText(ctx, 'Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + textLineHeight);
 
@@ -56,14 +81,19 @@ window.renderStatistics = function (ctx, names, times) {
     var columnXcoordinate = CLOUD_X + PADDING + columnLeftMargin;
     var columnHeight = playersTimeResults[i] * barHeight / maxTime;
     var columnYCoordinate = CLOUD_HEIGHT - columnHeight - (PADDING + TEXT_FONT_SIZE);
-    var chartColor = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(42, 0, 254, ' + Math.random() + ')';
-
+    var chartColor = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(42, 0, 254, ' + getRandomOpacity() + ')';
+    var chartColumnProps = {
+      color: chartColor,
+      x: columnXcoordinate,
+      y: columnYCoordinate,
+      width: BAR_WIDTH,
+      height: columnHeight,
+    };
     // выводим имена игроков
     renderText(ctx, names[i], columnXcoordinate, nameTextYcoordinate);
 
     // рисуем колонки
-    ctx.fillStyle = chartColor;
-    ctx.fillRect(columnXcoordinate, columnYCoordinate, BAR_WIDTH, columnHeight);
+    renderChartColumn(ctx, chartColumnProps);
 
     // выводим результат над колонками
     renderText(ctx, playersTimeResults[i], columnXcoordinate, columnYCoordinate - textLineHeight);
