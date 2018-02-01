6fa39c239ef7a76be80516058bbb0599d40fcabd
Артём <hoodsey20@gmail.com>
2018-02-01 20:38:53 +0700

    объявил константы, создал функцию для выдачи случайного значения из массива

--- js/setup.js
+++ js/setup.js
@@ -2,3 +2,49 @@
 
 var setupModalElement = document.querySelector('.setup');
 setupModalElement.classList.remove('hidden');
+
+var SIMILAR_WIZARD_QUANTITY = 4;
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+var WIZARD_LASTNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+
+var WIZARD_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)',
+];
+
+var WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green',
+];
+
+var getRandomValueFromArray = function (array) {
+  var randomIndex = Math.floor(Math.random() * (array.length));
+  return array[randomIndex];
+};
