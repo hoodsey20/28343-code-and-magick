5b20d8862e32ae9febc01a96533e316afd68b4fa
Артём <hoodsey20@gmail.com>
2018-02-11 13:33:49 +0700

    перенёс исходные данные в data.js

--- js/setup.js
+++ js/setup.js
@@ -2,56 +2,18 @@
 
 var SIMILAR_WIZARD_QUANTITY = 4;
 
-var wizardNames = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-];
 
-var wizardLastnames = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг',
-];
+var wizardCoatColorsForCustomize = window.data.wizardCoatColors.slice(0, window.data.wizardCoatColors.length - 1);
 
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)',
-];
-
-var wizardCoatColorsForCustomize = wizardCoatColors.slice(0, wizardCoatColors.length - 1);
-
-var wizardEyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green',
-];
-
-var wizardEyesColorsForCustomize = wizardEyesColors.slice(0, wizardEyesColors.length - 1);
+var wizardEyesColorsForCustomize = window.data.wizardEyesColors.slice(0, window.data.wizardEyesColors.length - 1);
 
 var setupSimilarElement = document.querySelector('.setup-similar');
 
 var generateWizard = function () {
   return {
-    name: window.util.getUniqueRandomValueFromArray(wizardNames) + ' ' + window.util.getUniqueRandomValueFromArray(wizardLastnames),
-    coatColor: window.util.getUniqueRandomValueFromArray(wizardCoatColors),
-    eyesColor: window.util.getUniqueRandomValueFromArray(wizardEyesColors),
+    name: window.util.getUniqueRandomValueFromArray(window.data.wizardNames) + ' ' + window.util.getUniqueRandomValueFromArray(window.data.wizardLastnames),
+    coatColor: window.util.getUniqueRandomValueFromArray(window.data.wizardCoatColors),
+    eyesColor: window.util.getUniqueRandomValueFromArray(window.data.wizardEyesColors),
   };
 };
 
@@ -85,14 +47,6 @@ setupSimilarElement.classList.remove('hidden');
 
 // код для взаимодействия пользователя с интерактивными элементами
 
-var wizardFireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848',
-];
-
 var wizardCoatElement = document.querySelector('.setup-wizard .wizard-coat');
 var wizardEyesElement = document.querySelector('.setup-wizard .wizard-eyes');
 var wizardFireballElement = document.querySelector('.setup-fireball-wrap');
@@ -106,7 +60,7 @@ var setEyesColor = function () {
 };
 
 var setFireballColor = function () {
-  wizardFireballElement.style.background = window.util.getRandomValueFromArray(wizardFireballColors);
+  wizardFireballElement.style.background = window.util.getRandomValueFromArray(window.data.wizardFireballColors);
 };
 
 wizardCoatElement.addEventListener('click', setCoatColor);
